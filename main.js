e016a3e4ae6e994e6d2d15ddf910d51d56a02cd1
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-06-08 00:43:49 +0630

    feat: Show modal and backdrop manipulating with js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const backdrop = document.querySelector('.backdrop');
+const plan_btns = document.querySelectorAll('.plan button');
+const modal = document.querySelector('.modal');
 
-backdrop.style.display = 'block';
+plan_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+})
